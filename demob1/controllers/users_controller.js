56923b1ce968bd36c7a6853ab8422eb99c729a87
chienpv <phamvanbk11@gmail.com>
2024-10-09 16:09:33 +0700

    validation

--- demob1/controllers/users_controller.js
+++ demob1/controllers/users_controller.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+var validator = require("validator");
+var _ = require("lodash");
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -14,6 +16,18 @@ class UsersController {
   static async create(req, res) {
     try {
       let { email, password } = req.body;
+      email = _.trim(email);
+      password = _.trim(password);
+
+      let validation = {};
+      let isvalidation = false;
+
+      if (!validator.isEmail(email)) {
+        validation.email = "Email không đúng định dạng!";
+      }
+
+      let islength = validator.isLength(password, { min: 5, max: 15 });
+
       password = bcrypt.hashSync(password, salt);
       let user = await User.create({ email, password });
       req.session.user = user;
